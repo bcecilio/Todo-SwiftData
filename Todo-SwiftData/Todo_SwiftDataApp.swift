2c964364a58a8a8fc81ad9d928292796db22b8bd
bcecilio <brendon.cecilio@gmail.com>
2026-01-04 11:49:08 -0800

    Initial Commit

---
+++ Todo-SwiftData/Todo_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_SwiftDataApp.swift
+//  Todo-SwiftData
+//
+//  Created by bjcecilio on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Todo_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Todo-SwiftDataTests/Todo_SwiftDataTests.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_SwiftDataTests.swift
+//  Todo-SwiftDataTests
+//
+//  Created by bjcecilio on 1/4/26.
+//
+
+import Testing
+@testable import Todo_SwiftData
+
+struct Todo_SwiftDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
